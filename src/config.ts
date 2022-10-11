94b5a91cdd12069fc4e152a33d45720aa9b8de23
Romeo Van Snick <romeovs@gmail.com>
2022-10-11 21:12:49 +0200

    Add config

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import { env } from "./env"
+
+export const PROJECT = env("BAKE_PROJECT")
+export const IMAGES = env("BAKE_IMAGES", "src/**/*.{jpeg,jpg}")
+export const CACHE = env("BAKE_CACHE", ".bake")
+
+export const SIZES = env("BAKE_SIZES", "512,1024,2048,3072,4096")
+export const FORMATS = env("BAKE_FORMATS", "jpeg,webp,avif")
+
+export const S3_ENDPOINT = env("S3_ENDPOINT")
+export const S3_BUCKET = env("S3_BUCKET")
+export const S3_ACCESS_KEY = env("S3_ACCESS_KEY")
+export const S3_SECRET_KEY = env("S3_SECRET_KEY")
+
+export const sizes = SIZES.split(",").map((str) => parseInt(str, 10))
+export const formats = FORMATS.split(",")

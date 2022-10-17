dd240d2fc552b7a3bd198719c72996c812ba41ac
Romeo Van Snick <romeovs@gmail.com>
2022-10-17 09:49:13 +0200

    Use vite to build

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
 build:
-	@esbuild --platform=node --target=node16 --format=cjs --bundle --outfile=dist/cmd.js --external:sharp src/cmd.ts
-	@esbuild --platform=node --target=node16 --format=cjs --bundle --outfile=dist/index.js --external:sharp --external:react --jsx=automatic src/index.tsx
-	@esbuild --platform=node --target=node16 --format=esm --bundle --outfile=dist/index.es.js --external:sharp --external:react --jsx=automatic src/index.tsx
+	@node vite.js
 
 run.bake:
 	@env node dist/cmd.js
---
+++ vite.js
@@ -0,0 +1,72 @@
+const path = require("path")
+const vite = require("vite")
+const dts = require("vite-plugin-dts")
+
+async function main() {
+	await vite.build({
+		mode: "production",
+		plugins: [
+			dts({
+				insertTypeEntry: true,
+				rollupTypes: true,
+				copyDtsFiles: false,
+			}),
+		],
+		build: {
+			outDir: "dist",
+			emptyOutDir: true,
+			target: "esnext",
+			sourcemap: true,
+			minify: false,
+			lib: {
+				entry: "./src/index.tsx",
+				formats: ["es", "cjs"],
+				fileName(format) {
+					if (format === "es") {
+						return "index.mjs"
+					} else {
+						return "index.cjs"
+					}
+				},
+			},
+			rollupOptions: {
+				external,
+			},
+		},
+	})
+
+	await vite.build({
+		mode: "production",
+		build: {
+			outDir: "dist",
+			emptyOutDir: false,
+			target: "es2015",
+			sourcemap: true,
+			minify: false,
+			lib: {
+				entry: "./src/cmd.ts",
+				formats: ["cjs"],
+				fileName: "bake",
+			},
+			rollupOptions: {
+				external,
+			},
+		},
+	})
+}
+
+main()
+
+function external(id, importer) {
+	if (!importer) {
+		// this is the entry
+		return false
+	}
+
+	if (!id.startsWith(".") && !path.isAbsolute(id)) {
+		// this is most a package name
+		return true
+	}
+
+	return false
+}

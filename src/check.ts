5aa6e9947ab51f6c223c48749b0b4a316479b3c5
Romeo Van Snick <romeovs@gmail.com>
2022-11-08 12:24:49 +0100

    Add logging to check

--- src/check.ts
+++ src/check.ts
@@ -7,12 +7,24 @@ export async function check() {
 	const queue = new Queue(5, 10000)
 	console.log("Collecting images...")
 	const requests = await matrix()
+	const total = requests.length
+	console.log(`Found ${total} image requests!`)
 	const promises = []
 
 	console.log("Checking S3...")
+
+	let idx = 0
 	for (const request of requests) {
 		const promise = queue.add(async function () {
 			const ok = await exists(request)
+			idx++
+
+			console.log(
+				`${idx.toString().padStart(5, " ")}/${total} ${request.file}\t\t${request.width}\t\t ${request.format} ${
+					ok ? "OK" : "NOT OK"
+				}`,
+			)
+
 			return {
 				...request,
 				ok,

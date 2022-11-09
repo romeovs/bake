3e622d9fc6aeae88cc0b5557f267d0d845756c57
Romeo Van Snick <romeovs@gmail.com>
2022-11-09 13:42:45 +0100

    Ignore empty files

--- src/exists.ts
+++ src/exists.ts
@@ -2,8 +2,8 @@ import { promises as fs } from "fs"
 
 export async function exists(filename: string): Promise<boolean> {
 	try {
-		await fs.access(filename)
-		return true
+		const f = await fs.stat(filename)
+		return f.size > 0
 	} catch (err) {
 		return false
 	}

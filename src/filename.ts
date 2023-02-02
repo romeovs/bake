fd605950bd60d1012c2aaa09ebd11fc3cee67543
Romeo Van Snick <romeovs@gmail.com>
2023-02-02 20:22:17 +1100

    Use same order in parse and filename

--- src/filename.ts
+++ src/filename.ts
@@ -1,33 +1,64 @@
-import { Format } from "./format"
 import { Request } from "./matrix"
 import { SrcInfo } from "./manifest"
 
 const base = 36
 const separator = "."
 
-export function filename(req: Request) {
-	return [
-		req.key,
-		req.hash,
-		req.width.toString(base),
-		req.height.toString(base),
-		req.quality.toString(base),
-		req.format,
-	].join(separator)
+type Def<T> = {
+	name: keyof Request
+	type: T
+}
+
+function string<K extends keyof Request>(name: Request[K] extends string ? K : never): Def<"string"> {
+	return { name, type: "string" }
+}
+
+function number<K extends keyof Request>(name: Request[K] extends number ? K : never): Def<"number"> {
+	return { name, type: "number" }
+}
+
+const order = [
+	string("key"),
+	string("hash"),
+	number("width"),
+	number("height"),
+	number("quality"),
+	string("format"),
+] as const
+
+export function filename(req: Request): string {
+	return order
+		.map(function (defn) {
+			const { name, type } = defn
+			const value = req[name]
+
+			if (type === "number") {
+				return value.toString(base)
+			}
+
+			return value
+		})
+		.join(separator)
 }
 
 export function parse(url: string): SrcInfo {
 	const parts = url.split("/")
-	const name = parts[parts.length - 1]
-	const items = name.split(separator)
+	const basename = parts[parts.length - 1]
+	const items = basename.split(separator)
+
+	const entries = order.map(function (defn, index) {
+		const { name, type } = defn
+		const value = items[index]
+
+		if (type === "number") {
+			return [name, parseInt(value, base)]
+		}
+
+		return [name, value]
+	})
 
 	return {
 		url,
-		key: items[0],
-		hash: items[1],
-		width: parseInt(items[2], base),
-		height: parseInt(items[3], base),
-		quality: parseInt(items[4], base),
-		format: items[5] as Format,
+		...Object.fromEntries(entries),
 	}
 }

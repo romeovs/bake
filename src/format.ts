49d2d302844a39a3f53118989413a1e5ace80b7a
Romeo Van Snick <romeo@romeovansnick.be>
2023-11-22 11:30:57 +0100

    Simplify formats

--- src/format.ts
+++ src/format.ts
@@ -1,5 +1,19 @@
-export type Format = "jpeg" | "webp" | "avif" | "png" | "gif"
+const formats = ["jpeg", "webp", "avif", "png", "gif"] as const
+
+export type Format = typeof formats[number]
 
 export function isFormat(format: string): format is Format {
-	return format === "jpeg" || format === "webp" || format === "avif" || format === "png" || format === "gif"
+	// @ts-expect-error
+	return formats.includes(format)
+}
+
+export function asFormat(format: string): Format {
+	if (isFormat(format)) {
+		return format
+	}
+	if (format === "jpg") {
+		return "jpeg"
+	}
+
+	throw new Error(`format not recognised: ${format}`)
 }
